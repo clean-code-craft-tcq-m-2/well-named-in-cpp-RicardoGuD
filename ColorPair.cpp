db34b3b052111ce16ab653f4d58810275faafb51
Ricardo Guerra <ricardo_gd2@hotmail.com>
2022-11-07 21:39:03 -0600

    Create ColorPair.cpp

---
+++ ColorPair.cpp
@@ -0,0 +1,29 @@
+#include "Colorpair.hpp"
+
+using namespace TelCoColorCoder;
+
+MajorColor ColorPair::getMajor() {
+    return majorColor;
+}
+
+MinorColor ColorPair::getMinor() {
+    return minorColor;
+}
+
+std::string ColorPair::ToString() {
+    std::string colorPairStr = MajorColorNames[majorColor];
+    colorPairStr += " ";
+    colorPairStr += MinorColorNames[minorColor];
+    return colorPairStr;
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
