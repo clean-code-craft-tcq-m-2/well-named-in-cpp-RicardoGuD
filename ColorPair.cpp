5a2a2543bd0587af3028df0b714b04bad1bcae16
Ricardo Guerra <ricardo_gd2@hotmail.com>
2022-11-08 18:03:22 -0600

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -11,19 +11,21 @@ MinorColor ColorPair::getMinor() {
 }
 
 std::string ColorPair::ToString() {
-    std::string colorPairStr = MajorColorNames[majorColor];
+    std::string colorPairStr = MajorColorNames[ static_cast<int>(majorColor)];
     colorPairStr += " ";
-    colorPairStr += MinorColorNames[minorColor];
+    colorPairStr += MinorColorNames[static_cast<int>(minorColor)];
     return colorPairStr;
 }
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-        return ColorPair(majorColor, minorColor);
-    }
+ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber)
+{
+    int zeroBasedPairNumber = pairNumber - 1;
+    MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return ColorPair(majorColor, minorColor);
+}
 
-int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;
-    }
+int TelCoColorCoder::GetPairNumberFromColor(MajorColor major, MinorColor minor)
+{
+    return static_cast<int>(major) * numberOfMinorColors + static_cast<int>(minor) + 1;
+}
