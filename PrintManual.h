4c6035c839389c4c1c8b01335374cb4e7da93c5f
Ricardo Guerra <ricardo_gd2@hotmail.com>
2022-11-08 20:26:28 -0600

    Create PrintManual.h

---
+++ PrintManual.h
@@ -0,0 +1,12 @@
+#ifndef PrintManual_h
+#define PrintManual_h
+
+#include "ColorPair.h"
+
+void printManual();
+
+void getColorNameSizes(int& majorColorSize, int& minorColorSize, TelCoColorCoder::ColorPair& colorPair);
+
+void fillSpaces(int numberofSpaces);
+
+#endif
